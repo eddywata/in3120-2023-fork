bff343b9041557cecddfcfa64adec0227a55c234
eddywata <edvardgulsvik@gmail.com>
2023-10-05 15:23:44 +0200

    got started on simplesearchengine

--- in3120/simplesearchengine.py
+++ in3120/simplesearchengine.py
@@ -44,4 +44,81 @@ class SimpleSearchEngine:
         N is inferred from the query via the "match_threshold" (float) option, and the maximum number of documents
         to return to the client is controlled via the "hit_count" (int) option.
         """
-        raise NotImplementedError("You need to implement this as part of the assignment.")
+        inverted_index = self.__inverted_index
+        query = list(inverted_index.get_terms(query))
+
+        m = len(query)
+        n = max(1, min(m, int(options['match_threshold'] * m)))
+
+        # collect posting lists for each term in query
+        postings_lists = []
+        for term in query:
+            print(term)
+            postings = inverted_index.get_postings_iterator(term)
+            postings_lists.append(postings)
+
+        sieve = Sieve(options['hit_count'])
+
+        # initial postings for all terms in query, will function as cursors throughout iteration
+        all_cursors = [next(p, None) for p in postings_lists]
+
+        print(all_cursors)
+        while all_cursors and len(all_cursors) >= n:
+            min_doc_id = min([cursor.document_id for cursor in all_cursors])
+
+            # the lowest document IDs mentioned in non-exhausted posting lists
+            frontier = [i for i, cursor in enumerate(all_cursors) if cursor is not None and cursor.document_id == min_doc_id]
+
+            if len(frontier) >= n:
+                scores = [cursor.term_frequency for cursor in (all_cursors[i] for i in frontier)]
+                for i, score in zip(frontier, scores):
+                    sieve.sift(score, all_cursors[i].document_id)
+            for i in frontier:
+                cursor = all_cursors[i] # TODO: unnecessary?
+                next_posting = next(postings_lists[i], None)
+                all_cursors[i] = next_posting
+
+            all_cursors = [cursor for cursor in all_cursors if cursor is not None]
+
+        # TODO: the loop may be alright. next is sieve winners, ranking and yielding?
+
+
+        #
+        # non_exhausted_lists = [i for i, cursor in enumerate(all_cursors) if cursor is not None]
+        #
+        # while non_exhausted_lists:
+        #     min_doc_id = min([cursor.document_id for cursor in non_exhausted_lists])
+        #     frontier = [cursor for cursor in non_exhausted_lists if cursor.document_id == min_doc_id]
+        #
+        #     if len(frontier) < n:
+        #         break
+        #
+        #     doc_id = all_cursors[frontier[0]][0]
+        #
+
+        # while all_cursors and len(all_cursors) >= n:
+        #     print(all_cursors is None)
+        #
+        #     min_doc_id = min([cursor.document_id for cursor in all_cursors if cursor is not None])
+        #     frontier = [i for i, cursor in enumerate(all_cursors) if cursor is not None if cursor.document_id == min_doc_id]
+        #
+        #     if len(frontier) >= n:
+        #         scores = [cursor.term_frequency for cursor in (all_cursors[i] for i in frontier)]
+        #         for i, score in zip(frontier, scores):
+        #             sieve.sift(score, all_cursors[i].document_id)
+        #
+        #     for i in frontier:
+        #         cursor = all_cursors[i]
+        #         next_posting = next(posting_lists[i], None)
+        #         all_cursors[i] = next_posting
+
+
+        # sieve = Sieve(options['hit_count'])
+        #
+        # top_results = sieve.winners()
+        #
+        # top_results = sorted(top_results, key=lambda x: x[0], reverse=True)
+        #
+        # for score, cursor in top_results:
+        #     document = self.__corpus.get_document(cursor)
+        #     yield {'score': score, 'document': document}
