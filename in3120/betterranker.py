3bddaa622fa0b041b4df8301ba5d1a7081e8a242
eddywata <edvardgulsvik@gmail.com>
2023-10-11 13:00:37 +0200

    implemented betterranker, passes all tests for assD-1

--- in3120/betterranker.py
+++ in3120/betterranker.py
@@ -5,6 +5,7 @@ from .ranker import Ranker
 from .corpus import Corpus
 from .posting import Posting
 from .invertedindex import InvertedIndex
+import math
 
 
 class BetterRanker(Ranker):
@@ -24,15 +25,33 @@ class BetterRanker(Ranker):
         self._document_id = None
         self._corpus = corpus
         self._inverted_index = inverted_index
-        self._dynamic_score_weight = 1.0  # TODO: Make this configurable.
-        self._static_score_weight = 1.0  # TODO: Make this configurable.
-        self._static_score_field_name = "static_quality_score"  # TODO: Make this configurable.
+        self._dynamic_score_weight = 0.0
+        self._static_score_weight = 0.0
+        self._static_score_field_name = "static_quality_score"
 
     def reset(self, document_id: int) -> None:
-        raise NotImplementedError("You need to implement this as part of the assignment.")
+        self._document_id = document_id
+        self._score = 0.0
+        try:
+            self._static_score_weight = self._corpus.get_document(
+                self._document_id).get_field(
+                self._static_score_field_name, 0.0)
+        # test_document_id_mismatch arbitrarily picks a docID that is bigger than the corpus,
+        # and so when we try to retrieve a static_quality_score we get an AssertionError
+        except AssertionError:
+            self._static_score_weight = 0.0
+        self._dynamic_score_weight = 0.0
+        self._score += self._static_score_weight
 
     def update(self, term: str, multiplicity: int, posting: Posting) -> None:
-        raise NotImplementedError("You need to implement this as part of the assignment.")
+        assert self._document_id == posting.document_id
+        tf = math.log(posting.term_frequency + 1, 10)
+        df = self._inverted_index.get_document_frequency(term)
+        n = self._corpus.size()
+        idf = math.log(n / df, 10)
+        tf_idf = tf * idf
+        self._dynamic_score_weight += multiplicity * tf_idf
+        self._score += self._dynamic_score_weight
 
     def evaluate(self) -> float:
-        raise NotImplementedError("You need to implement this as part of the assignment.")
+        return self._score
